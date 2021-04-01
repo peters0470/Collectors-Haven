5de3d8a55e687cfdd8afb1ffb5d18ef0146e4ea2
Shaggy <simmons5family@gmail.com>
2021-03-31 21:32:59 -0700

    start fresh from groud up, basic nav component

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <header>
+          <h1>
+            Collectors Haven
+          </h1>
+
+          <nav>
+            <ul>
+              <li>
+                Home
+              </li>
+              <li>
+                Sign In
+              </li>
+            </ul>
+          </nav>
+        </header>
+    )
+}
+
+export default Nav
