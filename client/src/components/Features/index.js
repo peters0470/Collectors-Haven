97d90372806a2c718b7e219d3cf837c91349dd3d
Shaggy <simmons5family@gmail.com>
2021-03-31 22:31:10 -0700

    add simple/basic Features component to Home page.

---
+++ client/src/components/Features/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+
+
+const Features = () => {
+    const features = [
+        {
+            "title": "Feature 1",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "class": "feature-container"
+        },
+        {
+            "title": "Feature 2",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "class": "feature-container"
+        },
+        {
+            "title": "Feature 3",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "class": "feature-container"
+        }
+    ]
+    return (
+        <div className='features-container'>
+        {features.map(feature => {
+          return (
+            <div className={`${feature.class}`}>
+              <h3>{feature.title}</h3>
+              <p>
+              {feature.desc}
+              </p>
+            </div>
+          )
+        })}
+        </div>
+    )
+}
+
+export default Features
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Reviews from '../components/Reviews'
+import Features from '../components/Features';
 
 const Home = () => {
     return (
         <div>
           <Reviews />
+          <Features />
         </div>
     )
 }
