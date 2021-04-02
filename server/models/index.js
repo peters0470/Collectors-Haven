6a80996d5aef860e0b35ef39aa0e475393fc4acb
Ayoubhm1993 <Ayoubhm1993@gmail.com>
2021-04-01 20:39:33 -0500

    models & db setup

---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+const User = require('./User');
+const Collection = require('./Collection');
+
+module.exports = { User, Collection};
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,43 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    collectionCount: Int
+    collection : [Collection]
+  }
+
+  type Item {
+    _id: ID
+    name: String
+    description: String
+    condition: String
+    priceBought: Int
+  }
+
+  type Collection {
+    name: String
+    user: [User]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    collections(name: String): [Collection]
+    item(name: String):[Item]
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addItem(name: String!, description: String!,condition: String!, priceBought: String!  ): Collection
+    addCollection(name: String!): Collection
+  }
+`;
+
+module.exports = typeDefs;
