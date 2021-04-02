fe767d21996545e92de6b6de985520c9f6a55a29
Shaggy <simmons5family@gmail.com>
2021-04-01 21:15:04 -0700

    add github links to footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,12 +1,53 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer>
-          <h2>Made By: Kevin, Ryan, Felix, Chris and Sarah</h2>
-          <p>&copy; 2021 Collectors Haven, Inc.</p>
-        </footer>
-    )
-}
+  return (
+    <footer>
+      <h2>
+        Made By:
+        <a
+          href="https://github.com/climbingryan"
+          target="_none"
+          className="link font-color"
+        >
+          Kevin 
+        </a>
+        ,
+        <a
+          href="https://github.com/peters0470"
+          target="_none"
+          className="link font-color"
+        >
+          Ryan 
+        </a>
+        ,
+        <a
+          href="https://github.com/fgutierrez2191"
+          target="_none"
+          className="link font-color"
+        >
+          Felix 
+        </a>
+        ,
+        <a
+          href="https://github.com/christroy"
+          target="_none"
+          className="link font-color"
+        >
+          Chris 
+        </a>
+        and 
+        <a
+          href="https://github.com/Sarah-McNeel"
+          target="_none"
+          className="link font-color"
+        >
+          Sarah 
+        </a>
+      </h2>
+      <p>&copy; 2021 Collectors Haven, Inc.</p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
