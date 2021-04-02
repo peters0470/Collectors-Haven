7c121917571a0926298fb1f767af26d05ce23655
Felix Gutierrez <fgutierrez2191@icloud.com>
2021-04-01 20:34:59 -0400

    added login.js functionality

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 
-const Login = () => {
+
+const Login = (props) => {
+  const [formState, setFormState] = useState({ username: '', password: '' });
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // clear form values
+    setFormState({
+      username: '',
+      password: '',
+    });
+  };
+
   return (
     <div className='signInForm-handler'>
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <div>
           <label htmlFor='username'>Username: </label>
           <input
@@ -12,6 +36,8 @@ const Login = () => {
             type="username"
             name="username"
             id="username"
+            value={formState.email}
+            onChange={handleChange}
           ></input>
         </div>
         <div>
@@ -21,6 +47,8 @@ const Login = () => {
             type="password"
             name="password"
             id="pwd"
+            value={formState.password}
+            onChange={handleChange}
           ></input>
         </div>
         <button className='signIn-btn'>Sign In</button>
