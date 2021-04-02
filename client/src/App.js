353f89a573d229952fbb5ef1e99995fe9b006303
Shaggy <simmons5family@gmail.com>
2021-04-01 17:19:29 -0700

    add basic Footer component to Home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './components/Header';
 import Home from './pages/Home';
+import Footer from './components/Footer';
 
 const App = () => {
     return (
         <>
           <Header />
           <Home />
+          <Footer />
         </>
     )
 }
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+          <h2>Made By: Kevin, Ryan, Felix, Chris and Sarah</h2>
+          <p>&copy; 2021 Collectors Haven, Inc.</p>
+        </footer>
+    )
+}
+
+export default Footer
