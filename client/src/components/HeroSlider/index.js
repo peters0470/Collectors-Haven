b3cd84e586c5d7670ce2388248ee5ac2a1013057
Shaggy <simmons5family@gmail.com>
2021-04-01 17:14:22 -0700

    edits to photo slider, still not working fully.

--- client/src/components/HeroSlider/index.js
+++ client/src/components/HeroSlider/index.js
@@ -2,31 +2,44 @@ import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 
-const slideImages = [
-  '../../assets/images/gaming-pc.jpg',
-  '../../assets/images/marioLand.jpg',
-  '../../assets/images/ps4Controller-purple.jpg',
-  '../../assets/images/retroPc.jpg',
-  '../../assets/images/vrAtSunset-purple.jpg'
-];
+
 
 const HeroSlider = () => {
+
+  const slideImages = [
+    '../../assets/images/gaming-pc.jpg',
+    '../../assets/images/marioLand.jpg',
+    '../../assets/images/ps4Controller-purple.jpg',
+    '../../assets/images/retroPc.jpg',
+    '../../assets/images/vrAtSunset.jpg'
+  ];
+
     return (
       <div className="slide-container">
         <Slide>
           <div className="each-slide">
-            <div style={{'background-image': `url(${slideImages[0]})`}}>
-              <span>Slide 1</span>
+            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+              <span>Gaming Pc</span>
             </div>
           </div>
           <div className="each-slide">
-            <div style={{'background-image': `url(${slideImages[1]})`}}>
-              <span>Slide 2</span>
+            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+              <span>Mario Land</span>
             </div>
           </div>
           <div className="each-slide">
-            <div style={{'background-image': `url(${slideImages[2]})`}}>
-              <span>Slide 3</span>
+            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+              <span>Playstation 4</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[3]})`}}>
+              <span>Retro</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[4]})`}}>
+              <span>Virtual Reality</span>
             </div>
           </div>
         </Slide>
