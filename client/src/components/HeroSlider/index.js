bfc4f2d25ccd0653728993479c7b5dfeff19a47d
Shaggy <simmons5family@gmail.com>
2021-04-02 18:44:07 -0700

    change arrows on image slider with arrows from font-awesome-fa

--- client/src/components/HeroSlider/index.js
+++ client/src/components/HeroSlider/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+
 import imgOne from '../../assets/images/gaming-pc.jpg'
 import imgTwo from '../../assets/images/marioLand.jpg';
 import imgThree from '../../assets/images/ps4Controller-purple.jpg';
@@ -19,34 +21,26 @@ const HeroSlider = () => {
     imgFive
   ];
 
+  const properties = {
+    duration: 5000,
+    transitionDuration: 500,
+    infinite: true,
+    prevArrow: <FaArrowLeft style={{ position: 'absolute', left: '50px', zIndex: 12, color: 'white' }} size='42px'/>,
+    nextArrow: <FaArrowRight style={{ position: 'absolute', left: '1825px', zIndex: 12, color: 'white' }} size='42px'/>,
+  }
+
     return (
-      <div className="slide-container">
-        <Slide>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
-
-            </div>
-          </div>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
-            </div>
-          </div>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
-
-            </div>
-          </div>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[3]})`}}>
-
-            </div>
-          </div>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[4]})`}}>
-
-            </div>
-          </div>
-        </Slide>
+      <div>
+        <div>
+          <Slide {...properties}>
+            {slideImages.map((each, index) => (
+              <div key={index} className="each-slide">
+                <div style={{ backgroundImage: `url(${each})` }}>
+                </div>
+              </div>
+            ))}
+          </Slide>
+        </div>
       </div>
     )
 }
