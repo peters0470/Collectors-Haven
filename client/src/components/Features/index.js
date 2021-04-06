8df484ec9f70f8b983876e5d5e3a7d8698eb13ca
Shaggy <simmons5family@gmail.com>
2021-04-05 23:02:35 -0700

    redo Features Component. Only 1 feature for now, will finish rest tomorrow.

--- client/src/components/Features/index.js
+++ client/src/components/Features/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import image from '../../assets/vector-images/Unsubscribed.png'
 
 const Features = () => {
     const features = [
@@ -21,8 +22,36 @@ const Features = () => {
             "titleColor": "feature-blue"
         }
     ]
+
+
     return (
-        <div className='features-container'>
+        <div className='feature-handler'>
+          <div className='section-one'>
+            <div className='img-one-backdrop'>
+              <img className='feature-img-one' src={image}>
+            </img>
+            </div>
+            <div className='feature-content-one'>
+              <h3>Feature #1</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
+        </div>
+    )
+}
+
+export default Features
+
+
+{/* <div className='features-container'>
         {features.map(feature => {
           return (
             <div className={`${feature.class}`}>
@@ -33,8 +62,4 @@ const Features = () => {
             </div>
           )
         })}
-        </div>
-    )
-}
-
-export default Features
+        </div> */}
