d4c92f9533992b637e41f8e3b4e56656c56d923a
Shaggy <simmons5family@gmail.com>
2021-04-05 23:07:24 -0700

    add spaces in footer between our names

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -8,42 +8,31 @@ const Footer = () => {
         <a
           href="https://github.com/climbingryan"
           target="_none"
-          className="link font-color"
-        >
-          Kevin 
-        </a>
-        ,
+          className="link font-color"> Kevin</a>
+        , 
         <a
           href="https://github.com/peters0470"
           target="_none"
           className="link font-color"
-        >
-          Ryan 
-        </a>
-        ,
+        > Ryan</a>
+        , 
         <a
           href="https://github.com/fgutierrez2191"
           target="_none"
           className="link font-color"
-        >
-          Felix 
-        </a>
-        ,
+        > Felix</a>
+        , 
         <a
           href="https://github.com/christroy"
           target="_none"
           className="link font-color"
-        >
-          Chris 
-        </a>
-        and 
+        > Chris </a>
+        and  
         <a
           href="https://github.com/Sarah-McNeel"
           target="_none"
           className="link font-color"
-        >
-          Sarah 
-        </a>
+        > Sarah </a>
       </h2>
       <p>&copy; 2021 Collectors Haven, Inc.</p>
     </footer>
